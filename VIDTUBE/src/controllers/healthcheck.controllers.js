90b86e4ff3f4eec5d2a37a21353509b2b312e1f6
yogendrasinh jadeja <meyogendrasinh.jadeja@gmail.com>
2024-08-03 15:39:45 +0530

    Unused code removed

--- VIDTUBE/src/controllers/healthcheck.controllers.js
+++ VIDTUBE/src/controllers/healthcheck.controllers.js
@@ -1,9 +1,6 @@
 import { apiResponse } from '../utils/apiResponse.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
 
-// const healthCheck = async (req, res) => {
-//     return res.status(200).json(new apiResponse(200, "OK", "Health check pass"))
-// }
 const healthCheck = asyncHandler(async (req, res) => {
     return res.status(200).json(new apiResponse(200, "OK", "Health check pass"))
 })
