4bac9a211461ec1db2002cfb211b5038158b1020
yogendrasinh jadeja <meyogendrasinh.jadeja@gmail.com>
2024-08-04 09:49:01 +0530

    Adde all mongoose models

--- VIDTUBE/src/controllers/healthcheck.controllers.js
+++ VIDTUBE/src/controllers/healthcheck.controllers.js
@@ -2,6 +2,7 @@ import { apiResponse } from '../utils/apiResponse.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
 
 const healthCheck = asyncHandler(async (req, res) => {
+    a= 10
     return res.status(200).json(new apiResponse(200, "OK", "Health check pass"))
 })
 
